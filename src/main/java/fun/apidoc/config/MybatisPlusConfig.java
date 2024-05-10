0595b5dfb0baa857b47b6ee3ec718830cea3dcbc
iboyang <lby1377@163.com>
2024-05-10 12:59:46 +0800

    增加mysql配置

---
+++ src/main/java/fun/apidoc/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package fun.apidoc.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author sanyuan027
+ * @date 2024年05月09日 10:36 下午
+ * desc:
+ */
+
+@Configuration
+@MapperScan("fun.apidoc.mapper")
+public class MybatisPlusConfig {
+
+    //分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
