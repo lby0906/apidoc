85b784988b0f1b7a463af8fed83f20c566774c5c
sanyuan <lby1377@163.com>
2024-05-22 16:05:03 +0800

    增加运行类

--- src/main/java/fun/apidoc/ApiDocApplication.java
+++ src/main/java/fun/apidoc/ApiDocApplication.java
@@ -1,9 +1,17 @@
 package fun.apidoc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author iboyang
  * @date 2024年05月07日 19:08
  * desc:
  */
+@SpringBootApplication
 public class ApiDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDocApplication.class,args);
+    }
 }
